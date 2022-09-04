20edd69bf7d279b34944d04f6550e8d57d7ad132
Souls-R <2362912722@qq.com>
2022-09-04 13:56:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,13 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#启用软件
+cat >> .config <<EOF
+CONFIG_PACKAGE_luci-app-argon-config=y
+EOF
+
+#启用主题
+cat >> .config <<EOF
+CONFIG_PACKAGE_luci-theme-argon=y
+EOF
