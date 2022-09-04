cc08db78c807a209bd4e8a5ab103a2a766382c7d
Souls-R <2362912722@qq.com>
2022-09-04 13:55:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,23 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
+# 删除旧主题
+rm -rf package/lean/luci-theme-argon
+
+rm -rf package/lean/luci-app-jd-dailybonus
+
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+
+#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb #git lua-maxminddb 依赖
+
+#git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+
+#git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+
+#git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
+
+#git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
