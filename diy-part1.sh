f1a261e166b251048dbc06b9fae7b40521bf91ce
Souls-R <2362912722@qq.com>
2022-09-11 09:28:45 +0000

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,25 +14,12 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 
 
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-
-# 删除旧主题
+# 替换默认主题为 luci-theme-argon
 rm -rf package/lean/luci-theme-argon
-
-rm -rf package/lean/luci-app-jd-dailybonus
-
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-
-#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb #git lua-maxminddb 依赖
-
-#git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
-
-#git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-
-#git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
-
-#git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
